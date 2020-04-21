1c8a174f543a77aac1360a2b66ecd841664ad487
s3ponia <slava.shlyaga@gmail.com>
2020-04-21 21:14:56 +0300

    fix

--- src/main/java/ru/mail/polis/s3ponia/DAOImpl.java
+++ src/main/java/ru/mail/polis/s3ponia/DAOImpl.java
@@ -6,49 +6,31 @@ import ru.mail.polis.Record;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class DAOImpl implements DAO {
 
-    private final SortedSet<Record> orderedRecords = new TreeSet<>();
-    private final Map<ByteBuffer, Record> keyToRecord = new HashMap<>();
+    private final SortedMap<ByteBuffer, Record> keyToRecord = new TreeMap<>();
 
     @NotNull
     @Override
     public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
-        final var record = keyToRecord.get(from);
-        if (record == null) {
-            return Collections.emptyIterator();
-        } else {
-            return orderedRecords.tailSet(keyToRecord.get(from)).iterator();
-        }
+        return keyToRecord.tailMap(from).values().stream().iterator();
     }
 
     @Override
     public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
-        var record = keyToRecord.get(key);
-        if (record != null) {
-            orderedRecords.remove(record);
-        }
-        record = Record.of(key, value);
+        var record = Record.of(key, value);
         keyToRecord.put(key, record);
-        orderedRecords.add(record);
     }
 
     @Override
     public void remove(@NotNull final ByteBuffer key) throws IOException {
         final var record = keyToRecord.remove(key);
-        orderedRecords.remove(record);
     }
 
     @Override
     public void close() throws IOException {
-        orderedRecords.clear();
         keyToRecord.clear();
     }
 }
